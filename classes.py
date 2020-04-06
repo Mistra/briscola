271a39710574c5bbfd442c6e1e24a1fcb81f4e01
ABVitali <alessandro.vitali1994@gmail.com>
2020-04-07 00:33:48 +0200

    added first files

---
+++ classes.py
@@ -0,0 +1,12 @@
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+class Card:
+    def __init__(self):
+        pass
+
+class Player:
+    def __init__(self):
+        self.cards = []
+
