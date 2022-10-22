1fc993bc202ed8dc2e26d6dc72582332e1609912
Mistra <sperectrum@gmail.com>
2022-10-22 23:45:29 +0000

    updated player entity

--- src/model/player.py
+++ src/model/player.py
@@ -6,8 +6,7 @@ class Player:
     created_at: None
     last_seen: None
 
-    def __init__(self, id, name, created_at=None, last_seen=None):
+    def __init__(self, id, name, created_at=None):
         self.id = id
         self.name = name
         self.created_at = created_at
-        self.last_seen = last_seen
