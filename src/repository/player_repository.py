ca5c20d1c399ca1f1aa9b8c493828f2cd078d0d3
Mistra <sperectrum@gmail.com>
2022-10-23 00:03:48 +0000

    implemented player repository with tests

--- src/repository/player_repository.py
+++ src/repository/player_repository.py
@@ -1,5 +1,7 @@
 
+
 import logging
+from datetime import datetime
 
 from src.model.player import Player
 from src.repository.db_factory import get_db_connection
@@ -8,36 +10,52 @@ from src.repository.db_factory import get_db_connection
 class PlayerRepository:
     database = None
 
-    def __init__(self):
-        self.database = get_db_connection()
+    def __init__(self, db_conn=None):
+        self.database = db_conn if db_conn is not None else get_db_connection()
 
     def save(self, player):
-        logging.debug("Saving player to the database %s", player.__dict__)
+        logging.debug("Saving player with id %s", player.id)
+
+        query = '''
+        INSERT OR REPLACE INTO player (
+            id,
+            name,
+            created_at
+        ) VALUES (?, ?, ?)
+        '''
+
         self.database.execute(
-            "INSERT INTO player (id, name, created_at, last_seen) VALUES (?, ?, ?, ?)",
-            (player.id, player.name, player.created_at, player.last_seen)
+            query,
+            (
+                player.id,
+                player.name,
+                player.created_at
+            )
         )
         self.database.commit()
         return player
 
-    def get_by_id(self, player_id):
-        logging.debug(
-            "Fetching player with id %s from the database", player_id)
-        row = self.database.execute(
-            "SELECT * FROM player WHERE id = ?", (player_id,)).fetchone()
+    def find_by_id(self, player_id):
+        logging.debug("Attempting to fetch player with id %s", player_id)
+
+        query = "SELECT * FROM player WHERE id = ?"
+        row = self.database.execute(query, (player_id,)).fetchone()
+
         if row is not None:
-            return Player(row[0], row[1], row[2], row[3])
+            return self.__row_to_player(row)
+
         return None
 
-    def get_all(self):
-        logging.debug("Fetching all players from the database")
-        rows = self.database.execute("SELECT * FROM player").fetchall()
-        # convert rows to Player objects
-        return [Player(row[0], row[1], row[2], row[3]) for row in rows]
-
     def delete_by_id(self, player_id):
-        logging.debug(
-            "Deleting player with id %s from the database", player_id)
-        self.database.execute(
-            "DELETE FROM player WHERE id = ?", (player_id,))
+        logging.debug("Attempting to delete player with id %s", player_id)
+
+        query = "DELETE FROM player WHERE id = ?"
+        self.database.execute(query, (player_id,))
         self.database.commit()
+
+    def __row_to_player(self, row):
+        player = Player()
+        player.id = row[0]
+        player.name = row[1]
+        player.created_at = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S')
+        return player
---
+++ test/repository/test_player_repository.py
@@ -0,0 +1,86 @@
+
+
+import re
+import sqlite3
+import unittest
+from datetime import datetime
+
+from src.model.player import Player
+from src.repository.player_repository import PlayerRepository
+
+
+class TestStackRepository(unittest.TestCase):
+    def setUp(self):
+        self.db_conn = sqlite3.connect(":memory:")
+        cursor = self.db_conn.cursor()
+
+        create_table_sql = None
+
+        # reading directly from schema definition
+        with (open("database/schema.sql", mode="r", encoding="utf-8")) as file:
+            schema = file.read()
+            regexp = "CREATE.*\\splayer\\s[^;]*;"
+            create_table_sql = re.search(regexp, schema).group()
+
+        cursor.execute(create_table_sql)
+        self.db_conn.commit()
+
+    def tearDown(self) -> None:
+        self.db_conn.close()
+
+    def test_create(self):
+        player = self.__dummy_player()
+        player_repository = PlayerRepository(self.db_conn)
+        player_repository.save(player)
+
+        query = "SELECT * FROM player"
+        row = self.db_conn.execute(query).fetchone()
+
+        self.assertEqual(row[0], player.id)
+        self.assertEqual(row[1], player.name)
+        self.assertEqual(row[2], "2000-01-01 00:00:00")
+
+    def test_update(self):
+        player = self.__dummy_player()
+        player_repository = PlayerRepository(self.db_conn)
+        player_repository.save(player)
+
+        player.name = "jim"
+        player_repository.save(player)
+
+        query = "SELECT * FROM player"
+        row = self.db_conn.execute(query).fetchone()
+
+        self.assertEqual(row[1], "jim")
+
+    def test_find_by_id(self):
+        player_repository = PlayerRepository(self.db_conn)
+        player = self.__dummy_player()
+
+        result = player_repository.find_by_id(player.id)
+        self.assertEqual(result, None)
+
+        player_repository.save(player)
+        result = player_repository.find_by_id(player.id)
+        self.assertEqual(result, player)
+
+    def test_delete_by_id(self):
+        player_repository = PlayerRepository(self.db_conn)
+        player = self.__dummy_player()
+        player_repository.save(player)
+        player_repository.delete_by_id(player.id)
+
+        result = player_repository.find_by_id(player.id)
+        self.assertEqual(result, None)
+
+    def __dummy_player(self) -> Player:
+        player = Player()
+        player.id = "123"
+        player.name = "nick"
+        player.created_at = datetime(2000, 1, 1, 0, 0, 0)
+
+        return player
+
+
+if __name__ == '__main__':
+    unittest.main()
